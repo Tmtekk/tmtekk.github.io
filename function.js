a9ca5935bcf1d10d9dd0fdf71471d32fee8e41fd
Tim <owner@Owners-MacBook-Pro.local>
2017-12-21 22:37:06 -0500

    added a javascript file for functionality

---
+++ function.js
@@ -0,0 +1,17 @@
+function toggleAboutMe() {
+    var x = document.getElementById("mainAbout");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function toggleExperiments() {
+    var x = document.getElementById("mainExperiments");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
