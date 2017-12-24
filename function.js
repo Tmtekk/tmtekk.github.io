91599178756866824a89ebf1a040786f51ed00ed
Tim <owner@Owners-MacBook-Pro.local>
2017-12-23 19:46:52 -0500

    many changes. added a resume file

--- function.js
+++ function.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function toggleAboutMe() {
     var x = document.getElementById("mainAbout");
     if (x.style.display === "block") {
@@ -26,10 +28,28 @@ function toggleContact() {
     else {
       x.style.display = "block";
     }
+    /*
+    if the resume is being displayed, and the contact link is
+    clicked, hide the resume section and display contact info.
+    */
+    var y = document.getElementById('mainResume');
+    if (y.style.display === "block")
+      y.style.display = "none";
 }
 
-// $(document).ready(function(){
-//     $("contact").click(function(){
-//         $("mainContact").fadeIn("slow");
-//     });
-// });
+function toggleResume() {
+    var x = document.getElementById("mainResume");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    }
+    else {
+      x.style.display = "block";
+    }
+    /*
+    if the contact is being displayed, and the resume link is
+    clicked, hide the contact section and display the resume.
+    */
+    var y = document.getElementById('mainContact');
+    if (y.style.display === "block")
+      y.style.display = "none";
+}
