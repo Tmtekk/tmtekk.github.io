c951bb182d16237b9acdd9bae8cc002977b13e65
Tim <owner@Owners-MacBook-Pro.local>
2017-12-23 19:04:25 -0500

    many changes

--- function.js
+++ function.js
@@ -1,17 +1,35 @@
 function toggleAboutMe() {
     var x = document.getElementById("mainAbout");
     if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
+      x.style.display = "none";
+    }
+    else {
+      x.style.display = "block";
     }
 }
 
 function toggleExperiments() {
     var x = document.getElementById("mainExperiments");
     if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
+      x.style.display = "none";
+    }
+    else {
+      x.style.display = "block";
     }
 }
+
+function toggleContact() {
+    var x = document.getElementById("mainContact");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    }
+    else {
+      x.style.display = "block";
+    }
+}
+
+$(document).ready(function(){
+    $("contact").click(function(){
+        $("mainContact").fadeIn("slow");
+    });
+});
